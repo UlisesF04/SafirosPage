91aee7d78f31cdbeb6c2ecedd81d49de88e8370f
UlisesF04 <ulifernandez2014@gmail.com>
2026-02-18 15:00:05 -0300

    Final Update

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const DatabaseUrl = "https://docs.google.com/spreadsheets/d/1YJkcSV8kYtZ2XSyfxGVsMjQnskgFsDOUqrkidNt-HRs/export?format=csv";
+const DatabaseUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQcwAEz4LRbsJ8ewcgHadiM-9QpLjRBHXocXPNdFbUkRGeiTJHv0wOsVRe6capGzvdsi9V7TO5PP9vr/pub?gid=0&single=true&output=csv";
 const mainUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQcwAEz4LRbsJ8ewcgHadiM-9QpLjRBHXocXPNdFbUkRGeiTJHv0wOsVRe6capGzvdsi9V7TO5PP9vr/pub?gid=996489562&single=true&output=csv";
-/*Create items for the catalog page*/
+const PremiumDatabaseUrl="https://docs.google.com/spreadsheets/d/e/2PACX-1vQcwAEz4LRbsJ8ewcgHadiM-9QpLjRBHXocXPNdFbUkRGeiTJHv0wOsVRe6capGzvdsi9V7TO5PP9vr/pub?gid=1895909268&single=true&output=csv"
+
+/*Create items for the Standard Catalog page*/
 fetch(DatabaseUrl)
     .then(res => res.text())
     .then(data => {
         const rows = data.trim().split("\n").slice(1);
-
         const productos = rows.map(row => {
         const [nombre, precio, stock, imagenURL] = row.split(",");
         return {
@@ -19,7 +20,7 @@ fetch(DatabaseUrl)
     })
     .catch(err => console.error("Error cargando la base:", err));
 function mostrarProductos(productos) {
-    const catalogo = document.querySelector(".clothes");
+    const catalogo = document.querySelector("#standard");
     productos.forEach(p => {
         catalogo.innerHTML += `
         <div class="item">
@@ -31,6 +32,38 @@ function mostrarProductos(productos) {
         `;
     });
 }
+
+/*Create items for Premium Cataloge page*/
+fetch(PremiumDatabaseUrl)
+    .then(res => res.text())
+    .then(data => {
+        const rows = data.trim().split("\n").slice(1);
+        const p_productos = rows.map(row => {
+        const [nombre, precio, stock, imagenURL] = row.split(",");
+        return {
+            nombre: nombre?.trim(),
+            precio: precio?.trim(),
+            stock: stock?.trim(),
+            imagenURL: imagenURL?.trim()
+        };
+        });
+        mostrarPremium(p_productos);
+    })
+    .catch(err => console.error("Error cargando la base:", err));
+function mostrarPremium(productos) {
+    const catalogo = document.querySelector("#premium");
+    productos.forEach(p => {
+        catalogo.innerHTML += `
+        <div class="item">
+            <img src="${p.imagenURL}" alt="${p.nombre}">
+            <h3>${p.nombre}</h3>
+            <p class="bold">${p.precio}</p>
+            <p>Stock: ${p.stock}</p>
+        </div>
+        `;
+    });
+}
+
 /*Change main images*/ 
 if (document.querySelector(".front_main")) {
 
